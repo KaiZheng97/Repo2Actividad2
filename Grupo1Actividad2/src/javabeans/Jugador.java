040a40b44b47c5a25235e568fef9390b3d8481fc
Sergio RL <sergiorl1989@gmail.com>
2023-02-17 18:53:15 +0100

    Ajuste formato clases

--- Grupo1Actividad2/src/javabeans/Jugador.java
+++ Grupo1Actividad2/src/javabeans/Jugador.java
@@ -3,9 +3,7 @@ package javabeans;
 public class Jugador {
 	
 	private int dorsal;
-
     private int numeroTarjetasAmarillas;
-
     private int numeroTarjetasRojas;
 
 	public int getDorsal() {
@@ -33,44 +31,25 @@ public class Jugador {
 	}
 	
 	
-	 
-
     public void ponerDorsal(int dorsal) {
 
-                  if(dorsal >= 1 && dorsal <= 30) {
-
-                                this.dorsal = dorsal;
-
-                  }else {
-
-                                this.dorsal = -1;
-
-                  }
-
+    	if(dorsal >= 1 && dorsal <= 30) {
+    		this.dorsal = dorsal;
+    	}else {
+    		this.dorsal = -1;
+    	}
     }
 
-   
-
     public boolean estaExpulsado() {
-
-                  boolean expulsado = false;                                                  
-
-                  if(numeroTarjetasAmarillas == 2) {
-
-                                expulsado = true;
-
-                  }                          
-
-                  if(numeroTarjetasRojas == 1) {
-
-                                expulsado = true;
-
-                  }                          
-
-                  return expulsado;
-
-    }    
-    
-    
+    	
+    	boolean expulsado = false;                                                  
+    	if(numeroTarjetasAmarillas == 2) {
+    		expulsado = true;
+    	}                          
+    	if(numeroTarjetasRojas == 1) {
+    		expulsado = true;
+    	}                          
+    	return expulsado;
+  }    
 
 }
