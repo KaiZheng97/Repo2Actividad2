df9172230f061a0373aa73f8aa12a789c5510db7
Sergio RL <sergiorl1989@gmail.com>
2023-02-19 12:17:10 +0100

    Documentada clase Jugador

--- Grupo1Actividad2/src/javabeans/Jugador.java
+++ Grupo1Actividad2/src/javabeans/Jugador.java
@@ -1,11 +1,36 @@
 package javabeans;
 
+
+	/**
+	 * En esta clase diseñaremos la base para crear objetos Jugador de fútbol, 
+	 * los cuales dispondrán de varios atributos y dos métodos que intercactúan sobre dichos objetos.
+	 * 
+	 * @author Sergio
+	 * @version 1.0
+	 *
+	 */
 public class Jugador {
+	//ATRIBUTOS DE CLASE
 	
+	/**
+	 * Dato de tipo entero que hace referencia al número de dorsal del que dispondrá el Jugador.
+	 */	
 	private int dorsal;
+	
+	/**
+	 * Dato de tipo entero que hace referencia al hecho de si el objeto Jugador
+	 * posee tarjetas amarillas y su cantidad. 
+	 */
     private int numeroTarjetasAmarillas;
+    
+    /**
+     * Dato de tipo entero que hace referencia al hecho de si el objeto Jugador
+	 * posee tarjetas rojas y su cantidad. 
+     */
     private int numeroTarjetasRojas;
 
+    
+    //GETTER Y SETTER
 	public int getDorsal() {
 		return dorsal;
 	}
@@ -30,6 +55,7 @@ public class Jugador {
 		this.numeroTarjetasRojas = numeroTarjetasRojas;
 	}
 	
+	//METODOS DE CLASE
 	
     public void ponerDorsal(int dorsal) {
 
